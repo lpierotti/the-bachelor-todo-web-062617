5f583db28fd9ee0226ca23e2db06a9bc58459d07
Luke Pierotti <lpierotti2@gmail.com>
2017-06-16 20:21:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,80 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |seasons, array|
+    if season == seasons
+      array.each do |person|
+        person.each do |key, value|
+          #  binding.pry
+          if key == "status"
+            if value == "Winner"
+              return person["name"].split(" ")[0]
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, array|
+    array.each do |person|
+      person.each do |key, value|
+        if key == "occupation"
+          if value == occupation
+            return person["name"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |person|
+      person.each do |key, value|
+        if key == "hometown"
+          if value == hometown
+            count +=1
+          end
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |person|
+      person.each do |key, value|
+        if key == "hometown"
+          if value == hometown
+            return person["occupation"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0
+  numberOfContestants = 0
+  data.each do |seasons, array|
+    if seasons == season
+      array.each do |person|
+        person.each do |key, value|
+          if key == "age"
+            ages += value.to_f
+            numberOfContestants += 1
+            #binding.pry
+          end
+        end
+      end
+    end
+  end
+  (ages / numberOfContestants.to_f).round
 end
